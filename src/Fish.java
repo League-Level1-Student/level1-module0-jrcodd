cf5fa3f86faca182650ce51a153be39ac4895f97
League <league@WTS8.attlocal.net>
2018-04-24 17:10:28 -0700

    fish family,

---
+++ src/Fish.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+
+	
+	public class Fish {
+		boolean foundtreasure = false;
+		String myColor;
+		int myAge;
+		public Fish(String color, int age){
+			myColor = color;
+			myAge = age;
+		}
+	
+	void explore() {
+		Random lucky = new Random(2);
+	int l=	lucky.nextInt(2);
+		if(l == 1) {
+			foundtreasure = true;
+		}
+		if(foundtreasure) {
+		System.out.println("Yay the fish found treasure!!");
+	}
+	
+	
+	
+}
+}
---
+++ src/fish_family.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class fish_family {
+	public static void main(String[] args) {
+		Fish fish1 = new Fish("blue", 3);
+		Fish fish2 = new Fish("red", 6);
+		fish1.explore();
+		fish2.explore();
+	}
+}
